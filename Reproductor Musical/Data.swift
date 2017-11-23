edac5f2701324ba95b11a105d312f5ecdaeb0c98
alumnos <alumnos@516-17.local>
2017-11-23 20:55:24 +0100

    wfdarw

---
+++ Reproductor Musical/Data.swift
@@ -0,0 +1,21 @@
+//
+//  Data.swift
+//  Reproductor Musical
+//
+//  Created by alumnos on 23/11/17.
+//  Copyright © 2017 santi. All rights reserved.
+//
+
+import Foundation
+
+let songs: [Song] = [Song(title: "The Diary Of Jane",
+                          artist: "Breaking Benjamin",
+                          album: "Phobia",
+                          audio: URL(fileURLWithPath: ""),
+                          image: #imageLiteral(resourceName: "album_cover_for_the_song_____diary_of_jane_____by_suni_by_suniarts-d54o4d5")),
+    Song(title: "Make me wanna Die",
+         artist: "The Pretty Reckless",
+         album: "Light me up",
+         audio: URL(fileURLWithPath: ""),
+         image: #imageLiteral(resourceName: "The_Pretty_Reckless_-_Make_Me_Wanna_Die"))
+]
