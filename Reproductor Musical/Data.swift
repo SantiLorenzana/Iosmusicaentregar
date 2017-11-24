9dd1d3eb61324bb8ae50bc0c964304aca57a825a
alumnos <alumnos@516-17.local>
2017-11-24 21:28:33 +0100

    TANTOS CAMBIOS

--- Reproductor Musical/Data.swift
+++ Reproductor Musical/Data.swift
@@ -12,10 +12,10 @@ let songs: [Song] = [Song(title: "The Diary Of Jane",
                           artist: "Breaking Benjamin",
                           album: "Phobia",
                           audio: URL(fileURLWithPath: ""),
-                          image: #imageLiteral(resourceName: "album_cover_for_the_song_____diary_of_jane_____by_suni_by_suniarts-d54o4d5")),
+                          image: #imageLiteral(resourceName: "download")),
     Song(title: "Make me wanna Die",
          artist: "The Pretty Reckless",
          album: "Light me up",
          audio: URL(fileURLWithPath: ""),
-         image: #imageLiteral(resourceName: "The_Pretty_Reckless_-_Make_Me_Wanna_Die"))
+         image: #imageLiteral(resourceName: "download-2"))
 ]
--- Reproductor Musical/PlayingViewController.swift
+++ Reproductor Musical/PlayingViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class PlayingViewController: UIViewController {
+    
+    
 
     @IBOutlet weak var Titleview: UILabel!
     
@@ -16,17 +18,90 @@ class PlayingViewController: UIViewController {
     
     @IBOutlet weak var Artistview: UILabel!
     
+    @IBOutlet weak var fondo: UIImageView!
+    
+    
+     var currentsong: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         currentsong = 0
+        newSong(songIndex: 0)
+        Imageview.alpha = 0
+        UIView.animate(withDuration: 1) { 
+            self.Imageview.alpha = 1
+        }
+        
+    }
+    func appear()  {
+        Imageview.alpha = 0
+        Titleview.alpha = 0
+        Artistview.alpha = 0
+        UIView.animate(withDuration: 0.6) {
+            self.Imageview.alpha = 1
+            self.Titleview.alpha = 1
+            self.Artistview.alpha = 1
+            self.Titleview.center = CGPoint(x:100, y:100)
+            self.Artistview.center = CGPoint(x:-100, y:-100)
+        }
+        
 
-        Titleview.text = songs[0].title
-        Artistview.text = songs[0].artist
-        Imageview.image = songs[0].image
+        
     }
     
+    @IBAction func ButtonBefore(_ sender: Any) {
+        if (currentsong > 0) {
+        currentsong-=1
+        newSong(songIndex: currentsong)
+            UIView.transition(with: view, duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+            appear()
+        }
+        
+    }
 
+    @IBAction func ButtonForward(_ sender: Any) {
+        if (currentsong < songs.count - 1) {
+            currentsong += 1
+            newSong(songIndex: currentsong)
+
+            UIView.transition(with: view, duration: 1, options: .transitionFlipFromRight, animations: nil, completion: nil)
+            appear()
+        }
+        
+       
+    }
+    func newSong(songIndex: Int){
     
+        Titleview.text = songs[songIndex].title
+        Artistview.text = songs[songIndex].artist
+        Imageview.image = songs[songIndex].image
+        fondo.image = songs[songIndex].image
 
-   
-
+        
+    
+    }
+    @IBAction func rightswipe(_ sender: UISwipeGestureRecognizer) {
+        if(sender.direction == .right){
+            
+            ButtonForward((Any).self)
+            
+        }
+    }
+    @IBAction func leftswipe(_ sender: UISwipeGestureRecognizer) {
+        if(sender.direction == .left){
+            
+            ButtonBefore((Any).self)
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
